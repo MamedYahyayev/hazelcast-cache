fc5ba70a00133b07073bcdf329dedef021387b61
Mamed Yahyayev <yahyayev2001@gmail.com>
2021-02-17 19:49:02 +0400

    initial commit

---
+++ src/main/java/az/maqa/spring/hazelcast/HazelcastCacheApplication.java
@@ -0,0 +1,13 @@
+package az.maqa.spring.hazelcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HazelcastCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HazelcastCacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/maqa/spring/hazelcast/HazelcastCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package az.maqa.spring.hazelcast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HazelcastCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
